4b67ad5d7541f2ef15529bd88e5b2b2604c05c43
Сергей Залыгин <cplusplusjs@yandex.ru>
2026-06-28 10:36:19 +0300

    add DependencyInjection to Application and Infrastructure layers
    - Create DependencyInjection.cs with Add*Services methods
    - Add Microsoft.Extensions.Configuration.Abstractions and
      Microsoft.Extensions.DependencyInjection.Abstractions packages
    - Register Application and Infrastructure services in Program.cs
    - Add AGENTS.md with project rules

--- content/Capi/Capi.Api/Program.cs
+++ content/Capi/Capi.Api/Program.cs
@@ -1,8 +1,13 @@
 using Capi.Api;
+using Capi.Application;
+using Capi.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApiServices(builder.Configuration);
+builder.Services
+    .AddApiServices(builder.Configuration)
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
